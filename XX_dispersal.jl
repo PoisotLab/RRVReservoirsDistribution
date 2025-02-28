59f1441b5cf4aeff93222f1dc7c6200e7b839263
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-28 16:04:06 -0500

    dispersal (essai)

--- XX_dispersal.jl
+++ XX_dispersal.jl
@@ -1,6 +1,17 @@
+using SpeciesDistributionToolkit
+using CairoMakie
+
 # Dispersal data from Schloss 2012 in km/year
 
-raccoon = 3.48
-skunk = 3.21
+v_raccoon = 3.48
+v_skunk = 3.21
 
 # TODO get current range, and expand by 20 years increments to get range limitation
+raccoon = SDMLayer("rasters/Procyon_lotor_historical.tif"; bandnumber=2) .== 1
+d_raccoon = pseudoabsencemask(DistanceToEvent, raccoon)
+
+skunk = SDMLayer("rasters/Mephitis_mephitis_historical.tif"; bandnumber=2) .== 1
+d_skunk = pseudoabsencemask(DistanceToEvent, skunk)
+
+heatmap(d_skunk .<= (v_skunk * 100))
+heatmap(d_raccoon .<= (v_raccoon * 100))
