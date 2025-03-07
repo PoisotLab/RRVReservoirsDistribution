756991e5eeb288d47c500d5df6cafd7b1b87b437
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-07 08:43:10 -0500

    use auto number of threads

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 species=("Procyon lotor" "Mephitis mephitis")
 
 for sp in "${species[@]}"; do
-    julia -t 6 --project 01_sdm.jl $sp
-    julia -t 6 --project 02_predictions.jl $sp
+    julia -t auto --project 01_sdm.jl $sp
+    julia -t auto --project 02_predictions.jl $sp
 done
