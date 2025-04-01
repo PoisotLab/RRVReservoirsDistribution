2dd68ebc56f5902d4bbcaf6f64fb0a7323b3fb3f
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-04-01 12:27:06 -0400

    NDRI

--- 03b_maps_bivariate.jl
+++ 03b_maps_bivariate.jl
@@ -9,6 +9,8 @@ QC = SpeciesDistributionToolkit.gadm("CAN", "Québec")
 # Palette functions
 include("S2_multivariate_palettes.jl")
 
+shapscale(S) = maximum(abs.(quantile(S, [0.05, 0.95]))) .* (-1, 1)
+
 scenarios = ["SSP126", "SSP245", "SSP370", "SSP585"]
 timeframes = ["2021-2040", "2041-2060", "2061-2080", "2081-2100"]
 
@@ -26,55 +28,34 @@ for scenario in scenarios, timeframe in timeframes
         range_mask.indices[k] = true
     end
 
-    # Prepare the bivariate map
-    nbins = 10
-
-    rbiv = ((x) -> round(Int64, x * (nbins-1)) + 1).(raccoon)
-    sbiv = ((x) -> round(Int64, x * (nbins-1)) + 1).(skunk)
-
-    stevens1 = (p0 = colorant"#e8e8e8", p1 = colorant"#c85a5a", p2 = colorant"#64acbe")
-    stevens2 = (p0 = colorant"#e8e8e8", p1 = colorant"#5ac8c8", p2 = colorant"#be64ac")
-    stevens3 = (p0 = colorant"#e8e8e8", p1 = colorant"#6c83b5", p2 = colorant"#73ae80")
-    stevens4 = (p0 = colorant"#e8e8e8", p1 = colorant"#c8b35a", p2 = colorant"#9972af")
-
-    colpal = stevens3
-
-    pal = _get_bivariate_colormap(; n_stops=nbins, colpal...)
-    smpal = _get_bivariate_colormap(; n_stops=3, colpal...)
-
-    lics = LinearIndices(pal)
-    bivlayer = similar(range_mask, Integer)
-    for k in keys(range_mask)
-        try
-            bivlayer[k] = lics[rbiv[k], sbiv[k]]
-        catch
-            bivlayer.indices[k] = false
-        end
-    end
-
     # Mask for either species
     r_either = mosaic(any, [r_raccoon, r_skunk])
     nodata!(r_either, false)
 
+    NDRI = (raccoon - skunk) / (raccoon + skunk)
+
     f = Figure(; size = (800, 700))
     ax = Axis(f[1, 1], aspect=DataAspect())
-    poly!(ax, QC, strokecolor=:black, color=colpal.p0, strokewidth=1)
-    heatmap!(ax, mask(bivlayer, r_either), colormap=[pal[l] for l in vec(lics)])
+    poly!(ax, QC, strokecolor=:black, strokewidth=1, color="#dfdfdf")
+    hm = heatmap!(ax, mask(NDRI, r_either), colormap=:balance, colorrange=shapscale(mask(NDRI, r_either)))
     hidedecorations!(ax)
     hidespines!(ax)
     tightlimits!(ax)
+    Colorbar(f[1, 1], hm, label="NDRI", alignmode=Inside(), width=Relative(0.4), valign=:bottom, halign=:right, tellheight=false, tellwidth=false, vertical=false)
     lines!(ax, QC, color=:black)
     ax_inset = Axis(f[1, 1],
         aspect = 1,
-        width=Relative(0.25),
-        height=Relative(0.25),
+        width=Relative(0.27),
+        height=Relative(0.27),
         halign=1.0,
         valign=1.0,
         xlabel = "Raccoon",
+        xlabelsize = 18,
+        ylabelsize = 18,
         ylabel = "Skunk",
     )
-    heatmap!(ax_inset, 1:3, 1:3, smpal)
-    scatter!(ax_inset, rescale(raccoon, ax_inset.xaxis.attributes.limits.val...), rescale(skunk, ax_inset.yaxis.attributes.limits.val...), color=:black, markersize=1, alpha=0.15)
+    lines!(ax_inset, [0.0, 1.0], [0.0, 1.0], color=:grey60, linestyle=:dash)
+    scatter!(ax_inset, raccoon, skunk, color=values(NDRI), markersize=1, alpha=0.15, colormap=:balance, colorrange=shapscale(mask(NDRI, r_either)))
     hideydecorations!(ax_inset, label=false)
     hidexdecorations!(ax_inset, label=false)
     tightlimits!(ax_inset)
