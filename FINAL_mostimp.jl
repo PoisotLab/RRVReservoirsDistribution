9eb8b2e10bba3807038c7483fa99851cb909c3a4
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-07-03 11:47:44 -0400

    cleanup shapley values

--- FINAL_mostimp.jl
+++ FINAL_mostimp.jl
@@ -14,13 +14,14 @@ lnames = layers(provider)
 descr = layerdescriptions(provider)
 
 function _fromshap(taxname)
+    # This is the list of TRUE variables (i.e. BIOCLIM codes)
     v = Int64[DelimitedFiles.readdlm("data/$(replace(taxname, " " => "_")).params")...]
+    # This is a map of INDICES, i.e. indices in the vector V, it goes from 1 to the number of variables
     S = [SDMLayer("rasters/02_predictions/$(replace(taxname, " " => "_"))_shapley.tif"; bandnumber=i) for i in eachindex(v)]
     mostimp = mosaic(argmax, map(x -> abs.(x), S))
-    present = sort(unique(values(mostimp)))
     contrib = sum.(map(x -> abs.(x), S))
     contrib ./= sum(contrib)
-    return (map=mostimp, vars=present, contrib=contrib, allvars=v)
+    return (map=mostimp, contrib=contrib, variables=v)
 end
 
 function _abbreviator(x)
@@ -42,13 +43,17 @@ Sm = _fromshap("Mephitis mephitis")
 
 _friendly_palette = [colorant"#0072B2", colorant"#56B4E9", colorant"#009E73", colorant"#F5C710", colorant"#E69F00", colorant"#D55E00"]
 
+# This gives the index in XX.variables for the most important variables
 mIp = partialsortperm(Sp.contrib, 1:3, rev=true)
 mIm = partialsortperm(Sm.contrib, 1:3, rev=true)
 
+# Indices of the true most important variables
+truvp = Sp.variables[mIp]
+truvm = Sm.variables[mIm]
+truv = unique(vcat(truvp, truvm))
+
 # Colors
-Cp = _friendly_palette[1:2:5]
-Cm = _friendly_palette[2:2:6]
-cpal = vcat(Cp, Cm)
+cpal = vcat(_friendly_palette)
 
 # Get some figure action going
 f = Figure(; size=(500, 400))
@@ -56,16 +61,18 @@ f = Figure(; size=(500, 400))
 # Raccoon map
 mp = Axis(f[1,1:2], aspect=DataAspect())
 pal = fill(colorant"#cccccc", 12)
-pal[mIp] .= Cp
+pal[mIp] .= cpal[1:3]
 heatmap!(mp, Sp.map, colormap=pal, colorrange=(1, 12))
 lines!(mp, QC, color=:black)
+f
 
 # Skunk map
 mm = Axis(f[2,1:2], aspect=DataAspect())
 pal = fill(colorant"#cccccc", 12)
-pal[mIm] .= Cm
+pal[mIm] .= vcat(cpal[1], cpal[4:5])
 heatmap!(mm, Sm.map, colormap=pal, colorrange=(1, 12))
 lines!(mm, QC, color=:black)
+f
 
 # Axes
 for ax in [mm, mp]
@@ -75,19 +82,17 @@ for ax in [mm, mp]
 end
 
 sp_uuid = Phylopic.imagesof("Procyon lotor"; items=1)
-silhouetteplot!(mp, -61.5, 59, sp_uuid; markersize=40, label="P. lotor", color=:black)
+silhouetteplot!(mp, -62, 59, sp_uuid; markersize=40, label="P. lotor", color=:black)
 sp_uuid = Phylopic.imagesof("Mephitis mephitis"; items=1)
-silhouetteplot!(mm, -61.5, 59, sp_uuid; markersize=40, label="M. mephitis", color=:black)
+silhouetteplot!(mm, -62, 59, sp_uuid; markersize=40, label="M. mephitis", color=:black)
 
 # Variable importance
-
-pstring = [descr[lnames[p]] for p in Sp.allvars[mIp]]
-mstring = [descr[lnames[p]] for p in Sm.allvars[mIm]]
-vstring = _abbreviator.(vcat(pstring, mstring))
+pstring = [descr[lnames[p]] for p in truv]
+vstring = _abbreviator.(pstring)
 
 Legend(
     f[1:2, 3],
-    [PolyElement(; color=cpal[i]) for i in 1:length(cpal)],
+    [PolyElement(; color=c) for c in cpal[1:length(truv)]],
     vstring;
     orientation=:horizontal,
     nbanks=6,
@@ -100,3 +105,29 @@ Legend(
 # Show the figure
 display(f)
 CairoMakie.save("figures/FINAL_shapmap.png", f)
+
+# Area
+area = cellarea(Sp.map)
+
+# Get the data for the table
+for i in 1:19
+    # i is the true bioclim code
+    strings = [descr[lnames[i]]]
+    if i in Sp.variables
+        v_idx = findfirst(isequal(i), Sp.variables)
+        push!(strings, string(round(Sp.contrib[v_idx]; digits=2)))
+        v_area = sum(mask(area, nodata(Sp.map .== v_idx, false))) * 1e-3
+        push!(strings, string(round(v_area; digits=2)))
+    else
+        append!(strings, ["", ""])
+    end
+    if i in Sm.variables
+        v_idx = findfirst(isequal(i), Sm.variables)
+        v_area = sum(mask(area, nodata(Sm.map .== v_idx, false))) * 1e-3
+        push!(strings, string(round(Sm.contrib[v_idx]; digits=2)))
+        push!(strings, string(round(v_area; digits=2)))
+    else
+        append!(strings, ["", ""])
+    end
+    @info strings
+end
